ea94c29f80bc8a51ec51e123d8ee0477af50ae91
Michael Bayne <mdb@samskivert.com>
2001-07-13 23:25:13 +0000

    Mo' betta progress.

---
+++ src/java/com/samskivert/viztool/layout/CascadingConstants.java
@@ -0,0 +1,23 @@
+//
+// $Id$
+
+package com.samskivert.viztool.viz;
+
+/**
+ * Constants used in laying out chains in the cascading style.
+ *
+ * @see CascadingChainLayout
+ * @see CascadingChainRenderer
+ */
+public interface CascadingConstants
+{
+    /**
+     * The number of points surrounding the name of the chain.
+     */
+    public static final int HEADER_BORDER = 3;
+
+    /**
+     * The number of points of spacing between each child chain.
+     */
+    public static final int GAP = 4;
+}
---
+++ src/java/com/samskivert/viztool/layout/Element.java
@@ -0,0 +1,41 @@
+//
+// $Id$
+
+package com.samskivert.viztool.viz;
+
+import java.awt.Point;
+import java.awt.Dimension;
+
+/**
+ * A page is composed of elements which have some rectangular dimension.
+ * They can be laid out by <code>ElementLayout</code> implementations in a
+ * general purpose way.
+ */
+public interface Element
+{
+    /**
+     * Returns the name of this element.
+     */
+    public String getName ();
+
+    /**
+     * Returns the size of this element. All coordinates are in points.
+     */
+    public Dimension getSize ();
+
+    /**
+     * Sets the upper left position of this element. All coordinates are
+     * in points.
+     */
+    public void setLocation (int x, int y);
+
+    /**
+     * Sets the page number of this element.
+     */
+    public void setPage (int pageno);
+
+    /**
+     * Returns the page number previously set by <code>setPage</code>.
+     */
+    public int getPage ();
+}
