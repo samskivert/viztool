d2723020f50ff83f6c445cf9ca6d128d2631b597
Michael Bayne <mdb@samskivert.com>
2001-08-14 00:58:15 +0000

    Switched to using a Pageable in hopes that this would cause Java to allow the user to print only particular pages, but it doesn't seem to make use of the pageable information to do that. Alas.

--- src/java/com/samskivert/viztool/VizController.java
+++ src/java/com/samskivert/viztool/VizController.java
@@ -60,9 +60,26 @@ public class VizController extends Controller
             return true;
 
         } else if (cmd.equals(PRINT)) {
-            // tell the job to print our visualizer with the format we set
-            // up earlier
-            _job.setPrintable(_vpanel.getVisualizer(), _format);
+            // create a pageable to be used by our print job that does the
+            // right thing
+            Pageable pable = new Pageable()
+            {
+                public int getNumberOfPages ()
+                {
+                    return _vpanel.getVisualizer().getPageCount();
+                }
+
+                public PageFormat getPageFormat (int pageIndex)
+                {
+                    return _format;
+                }
+
+                public Printable getPrintable (int pageIndex)
+                {
+                    return _vpanel.getVisualizer();
+                }
+            };
+            _job.setPageable(pable);
 
             // pop up a dialog to control printing
             if (_job.printDialog()) {
